68318cc4be78e9620244e6a08c7ba9e35f66d30c
JaHIY <jaklsy@gmail.com>
2013-02-03 11:44:13 +0800

    beautify code

--- zhconverter.sh
+++ zhconverter.sh
@@ -6,35 +6,45 @@ eval set -- "$ARGV"
 print_usage() {
     printf '%s\n' \
             "Usage: ${0} [OPTION...] [FILE...]" \
-            'Convert Simplified Chinese to Traditional Chinese or convert Traditional Chinese to Simplified Chinese by MediaWiki API.' \
+            'Convert Simplified Chinese to Traditional Chinese' \
+            'or convert Traditional Chinese to Simplified Chinese by MediaWiki API.' \
             '' \
             'Options:' \
-            ' -t, --to-code=NAME        Set langage for output! at this you can use zh-cn, zh-hans, zh-hant, zh-hk, zh-sg or zh-tw(default)' \
-            ' -h, --help                 Show this help list.'
+            ' -t, --to-code=NAME        Set langage for output! at this you can use zh-cn,' \
+            '                           zh-hans, zh-hant, zh-hk, zh-sg or zh-tw(default)' \
+            ' -h, --help                Show this help list.'
 }
 
 print_error() {
     local my_name="${0}"
-    printf "${my_name}: missing optstring argument\nTry \`${my_name} --help' for more information.\n" 1>&2
+    printf '%s\n' \
+            "${my_name}: missing optstring argument" \
+            "Try \`${my_name} --help' for more information." 1>&2
 }
 
 convert_chinese() {
-    local AWK_SYNTAX='BEGIN{print "<pre>-{}-"}{print $0}END{print "</pre>"}'
-    local MEDIAWIKI_API='http://zh.wikipedia.org/w/api.php'
-    local MEDIAWIKI_DATA='action=parse&format=json&prop=text&disablepp=true&uselang='
-    local MEDIAWIKI_RETURN_SUBSTITUTE='s/^.*"text":{"\*":"<pre>\\n\(.*\)\\n<\\\/pre>\\n"}}}$/\1/'
-    local MEDIAWIKI_USELANG="${1}"
+    local awk_syntax='BEGIN{print "<pre>-{}-"}{print $0}END{print "</pre>"}'
+    local mediawiki_api='http://zh.wikipedia.org/w/api.php'
+    local mediawiki_data='action=parse&format=json&prop=text&disablepp=true&uselang='
+    local mediawiki_return_substitute='s/^.*"text":{"\*":"<pre>\\n\(.*\)\\n<\\\/pre>\\n"}}}$/\1/'
+    local mediawiki_uselang="${1}"
     shift
-    printf '%b' $(sed -e 's/&/\&amp;/;' -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/{/\&#123;/g' -e 's/}/\&#125;/g' "$@" | awk "$AWK_SYNTAX" | curl -s -d "${MEDIAWIKI_DATA}${MEDIAWIKI_USELANG}" --data-urlencode "text@-" "$MEDIAWIKI_API" | sed -e "$MEDIAWIKI_RETURN_SUBSTITUTE" -e 's/&#125;/}/g' -e 's/&#123;/{/g' -e 's/&gt;/>/g' -e 's/&lt;/</g' -e 's/&amp;/\&/;') | sed -e 's;\\\/;/;g'
+    printf '%b' \
+                $(sed -e 's/&/\&amp;/;' -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/{/\&#123;/g' -e 's/}/\&#125;/g' "$@" | \
+                awk "$awk_syntax" | \
+                curl -s -d "${mediawiki_data}${mediawiki_uselang}" --data-urlencode "text@-" "$mediawiki_api" | \
+                sed -e "$mediawiki_return_substitute" -e 's/&#125;/}/g' -e 's/&#123;/{/g' \
+                    -e 's/&gt;/>/g' -e 's/&lt;/</g' -e 's/&amp;/\&/;') | \
+   sed -e 's;\\\/;/;g'
 }
 
 main() {
-    local MEDIAWIKI_USELANG='zh-tw'
+    local mediawiki_uselang='zh-tw'
     while true
     do
         case "${1}" in
             '-t'|'--to-code')
-                MEDIAWIKI_USELANG="${2}"
+                mediawiki_uselang="${2}"
                 shift 2
                 ;;
             '-h'|'--help')
@@ -45,7 +55,7 @@ main() {
                 shift
                 if [ $# -gt 0 ]
                 then
-                    convert_chinese "$MEDIAWIKI_USELANG" "$@"
+                    convert_chinese "$mediawiki_uselang" "$@"
                     break
                 else
                     print_error
